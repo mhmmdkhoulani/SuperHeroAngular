01567e8f1c84ecad1f152d295c8a5d74b637bfcd
Mohammad Khoulani <mhmmdkholani@gmail.com>
2023-06-01 14:13:50 +0300

    create and connect database

---
+++ SuperHero.Api/SuperHero.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHero.Api.Models;
+
+namespace SuperHero.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHeroModel> SuperHeros { get; set; }
+    }
+}
--- SuperHero.Api/SuperHero.Api/Program.cs
+++ SuperHero.Api/SuperHero.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHero.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigin", policy =>
 {
     policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
