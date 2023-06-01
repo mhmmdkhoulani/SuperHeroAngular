91759d5a78853c66966fc68912a36bb51134aa1d
Mohammad Khoulani <mhmmdkholani@gmail.com>
2023-06-01 13:21:30 +0300

    Create first end point to get heros in api

---
+++ SuperHero.Api/SuperHero.Api/Controllers/SuperHeorsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHero.Api.Models;
+
+namespace SuperHero.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeorsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHeroModel>>> GetSuperHeros()
+        {
+            var heros =  new List<SuperHeroModel> { 
+                new SuperHeroModel 
+                {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Pareker",
+                    Place ="New York City"
+                },
+            };
+
+            return heros;
+        }
+    }
+}
